3c29311ef3cfed36d0d69d065023d6c46cf880cf
Edgar H Salinas <edgar@my.codergv.org>
2017-09-06 21:47:04 -0500

    First Commit

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+// Index.ios.js - place code in here for iOS
+
+// Import a library to help create a component
+
+// Create a component 
+
+
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header'
+
+const App = () => (
+    <Header /> // Component Nesting
+  );
+
+// It looks better above then previous code below
+
+// const App = () => {
+//   return (
+//     <Text>Test Text</Text>
+//   );
+
+// };
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
