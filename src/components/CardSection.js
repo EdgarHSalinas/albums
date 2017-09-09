1a81257cefc0d8ca37a6502499627dd5cc68119f
Edgar H Salinas <edgar@my.codergv.org>
2017-09-09 17:54:52 -0500

    Card section styles was not working, typo

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CardSection = (props) => {
-    return (
-        <View style={styles.containerStyles}>
-            {props.children}
-        </View>
-    );
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
 };
 
 const styles = {
     containerStyle: {
-        borderBottomWidth: 1,
-        padding: 5,
-        backgroundColor: '#fff',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        borderColor: '#ddd',
-        position: 'relative'
+      borderBottomWidth: 1,
+      padding: 5,
+      backgroundColor: '#fff',
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+      borderColor: '#ddd',
+      position: 'relative'
     }
 };
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = (props) => {
@@ -29,7 +29,7 @@ const styles = {
     textStyle: {
         fontSize: 20
     }
-}
+};
 
 // Make the component available to other parts of the app
 export default Header;
